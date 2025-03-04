c8d99f0fe7b810eb1303b0e07d1b5e0979ecaacc
THAYORZZ\thatc <thatcharnon.10210@gmail.com>
2025-03-05 04:10:18 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+<div align="center">
+  <a href="https://github.com/thayorch/204114-Project/">
+    <img src="logo.png" alt="Logo" width="80" height="80">
+  </a>
+  <h3 align="center">Pew Pew!</h3>
+  <p align="center">
+    The Demo Online Games projects
+    <br />
+  <a href="https://www.google.com/search?rls=en&q=chiang+mai+university">Chiang Mai University</a></strong> , Thailand    
+  </p>
+<!-- 
+  <a href="https://github.com/thayorch/204114-Project/releases/download/PewPew-1.0/Game.jar" target="_blank">
+        <img src="https://img.shields.io/static/v1?message=Download&logo=&label=&color=1f72bf&logoColor=&labelColor=&style=for-the-badge" alt="download"  />
+  </a> -->
+</div>
---
+++ index.js
@@ -0,0 +1,69 @@
+require("dotenv").config();
+const axios = require("axios");
+const express = require("express");
+const { userInfo } = require("os");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("static"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/static/index.html"));
+});
+
+app.get("/auth", (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
+
+app.get("/oauth-callback", ({ query: { code } }, res) => {
+  const body = {
+    client_id: process.env.GITHUB_CLIENT_ID,
+    client_secret: process.env.GITHUB_SECRET,
+    code,
+  };
+  const opts = { headers: { accept: "application/json" } };
+
+  axios
+    .post("https://github.com/login/oauth/access_token", body, opts)
+    .then((_res) => {
+      // console.log("GitHub Response:", _res.data); // Log the response
+      return _res.data.access_token;
+    })
+    .then((token) => {
+      if (!token) {
+        throw new Error("Token not found in GitHub response");
+      }
+      // console.log("My token:", token);
+
+      // Fetch user details using the access token
+      return axios.get("https://api.github.com/user", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    })
+    .then((userRes) => {
+      const userDetails = userRes.data;
+      // console.log("User Details:", userDetails);
+      console.log(`[log] : User[${userDetails.login}] is Login`);
+      
+      // Redirect to the home page with user details as query parameters
+      const queryParams = new URLSearchParams({
+        username: userDetails.login,
+        avatar_url: userDetails.avatar_url,
+        name: userDetails.name || userDetails.login,
+      }).toString();
+      res.redirect(`/?${queryParams}`);
+    })
+    .catch((err) => {
+      console.error("Error:", err.message); // Log the error
+      res.status(500).json({ err: err.message });
+    });
+});
+
+app.listen(3000);
+// eslint-disable-next-line no-console
+console.log("App listening http://localhost:3000/");
